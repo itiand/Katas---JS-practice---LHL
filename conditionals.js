46a4a8a53eb3d43fadc87e0f0b3a1f1c0a477099
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-05-01 11:15:06 -0400

    add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -9,3 +9,18 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+
+const raining = true;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
